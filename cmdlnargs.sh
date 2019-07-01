f9e0fdd53b8d356019bc92d0adfeb243578ef9cf
MMuniraja <37608051+MMuniraja@users.noreply.github.com>
2019-07-01 08:01:08 +0530

    Create cmdlnargs.sh

---
+++ cmdlnargs.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "The first Parameter is $1"
+echo "The second Parameter is $2"
+echo "The Parameters are: $*"
+echo "The Parameters are: $@"
+echo "The no.of Parameters are: $#"
+echo "The script name is: $0"
