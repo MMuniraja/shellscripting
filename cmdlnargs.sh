c848b1749bf45d15a433464047d029f50d3b6922
MMuniraja <37608051+MMuniraja@users.noreply.github.com>
2019-07-14 14:54:02 +0530

    Update cmdlnargs.sh

--- cmdlnargs.sh
+++ cmdlnargs.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 echo "The first Parameter is $1"
 echo "The second Parameter is $2"
+echo " The addition of two parameters is : `expr $1 + $2`"
 echo "The Parameters are: $*"
 echo "The Parameters are: $@"
 echo "The no.of Parameters are: $#"
 echo "The script name is: $0"
+echo " The process id is: $$"
+echo "The exit status of prev. cmd is : $?"
+echo "The process id of background running process: $!"
