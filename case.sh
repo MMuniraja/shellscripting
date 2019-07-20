ead19d556bc7f44397cff9b9d3563fc5186f243b
MMuniraja <37608051+MMuniraja@users.noreply.github.com>
2019-07-20 15:34:38 +0530

    Create case.sh

---
+++ case.sh
@@ -0,0 +1,11 @@
+case $1 in
+car) echo "The car rental is 100/hr"
+        ;;
+bike) echo "The bike rental is 50/hr"
+        ;;
+cycle) echo "The cycle rental is  30/hr"
+        ;;
+*) echo "**Unknwn Vehicle**"
+        ;;
+
+esac
