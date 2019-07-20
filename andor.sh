aec281183e762f8330ee9bc5adfb1d55f255e428
MMuniraja <37608051+MMuniraja@users.noreply.github.com>
2019-07-20 14:49:36 +0530

    Update andor.sh

--- andor.sh
+++ andor.sh
@@ -10,3 +10,11 @@ then
 else
 	echo "Not OK"
 fi
+read -p "Enter Your Number:"  i
+
+if [ ( $i -ge 10  -a  $i -le 20 ) -o ( $i -ge 100  -a  $i -le 200 ) ]
+then
+        echo "OK"
+else
+	echo "Not OK"
+fi
